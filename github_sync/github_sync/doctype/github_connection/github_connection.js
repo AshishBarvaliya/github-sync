43a09514333094a81a16f587e3460aa4d658893a
Ashish Baravaliya <vier.institute35@gmail.com>
2024-01-28 10:51:34 -0500

    feat: do not allow update once connection created

--- github_sync/github_sync/doctype/github_connection/github_connection.js
+++ github_sync/github_sync/doctype/github_connection/github_connection.js
@@ -12,54 +12,66 @@ frappe.ui.form.on("Github Connection", {
         );
       })
       .addClass("btn-primary");
+
+    if (!frm.is_new()) {
+      frm.set_df_property("github_user", "read_only", 1);
+      frm.set_df_property("repository", "read_only", 1);
+      frm.set_df_property("project", "read_only", 1);
+      frm.set_df_property("github_login", "hidden", 1);
+      frm.set_df_property("github_logout", "hidden", 1);
+    }
   },
   async onload(frm) {
-    const access_token = frappe.get_cookie("github_access_token");
-    if (access_token) {
-      const headers = {
-        Authorization: `token ${access_token}`,
-      };
-      const response = await fetch("https://api.github.com/user/repos", {
-        headers,
-      });
-      if (!response.ok) {
-        frappe.msgprint({
-          title: __("Error"),
-          message: __("Invalid access token. Please login again."),
-          indicator: "red",
+    if (frm.is_new()) {
+      const access_token = frappe.get_cookie("github_access_token");
+      if (access_token) {
+        const headers = {
+          Authorization: `token ${access_token}`,
+        };
+        const response = await fetch("https://api.github.com/user/repos", {
+          headers,
         });
-        return;
+        if (!response.ok) {
+          frappe.msgprint({
+            title: __("Error"),
+            message: __("Invalid access token. Please login again."),
+            indicator: "red",
+          });
+          return;
+        }
+        const userResponse = await fetch("https://api.github.com/user", {
+          headers,
+        });
+        const userData = await userResponse.json();
+        const repos = await response.json();
+        if (repos.length < 0) {
+          frappe.msgprint("No repositories found");
+          return;
+        }
+        frm.set_value("github_user", userData.login);
+        const options = repos.map((repo) => repo.name);
+        frm.set_df_property("repository", "options", options);
+        frm.refresh_field("repository");
+        frm.refresh_field("github_user");
       }
-      const userResponse = await fetch("https://api.github.com/user", {
-        headers,
-      });
-      const userData = await userResponse.json();
-      const repos = await response.json();
-      if (repos.length < 0) {
-        frappe.msgprint("No repositories found");
-        return;
-      }
-      frm.set_value("github_user", userData.login);
-      const options = repos.map((repo) => repo.name);
-      frm.set_df_property("repository", "options", options);
-      frm.refresh_field("repository");
-      frm.refresh_field("github_user");
     }
   },
   async github_login(frm) {
-    const response = await frappe.call({
-      method: "github_sync.github_sync.api.get_config",
-      args: { key: "github_client_id" },
-    });
-    const CLIENT_ID = response.message;
-    const REDIRECT_URI = `${window.location.origin}/api/method/github_sync.github_sync.api.callback`;
-    const SCOPES = "repo";
-    window.open(
-      `https://github.com/login/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&scope=${encodeURIComponent(
-        SCOPES
-      )}`,
-      "_self"
-    );
+    if (frm.is_new()) {
+      const response = await frappe.call({
+        method: "github_sync.github_sync.api.get_config",
+        args: { key: "github_client_id" },
+      });
+      const CLIENT_ID = response.message;
+      const REDIRECT_URI = `${window.location.origin}/api/method/github_sync.github_sync.api.callback`;
+      const SCOPES = "repo";
+      window.open(
+        `https://github.com/login/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&scope=${encodeURIComponent(
+          SCOPES
+        )}`,
+        "_self"
+      );
+    }
   },
   github_logout() {
     frappe.call({
@@ -67,34 +79,36 @@ frappe.ui.form.on("Github Connection", {
     });
   },
   async before_save(frm) {
-    const github_access_token = frappe.get_cookie("github_access_token");
-    const webhookSecret =
-      Math.random().toString(36).substring(2, 15) + +new Date();
-    const webhookData = {
-      name: "web",
-      active: true,
-      events: ["issues", "issue_comment", "label"],
-      config: {
-        url: `${window.location.origin}/api/method/github_sync.github_sync.api.webhook`,
-        content_type: "json",
-        insecure_ssl: "0",
-        secret: webhookSecret,
-      },
-    };
-
-    const response = await fetch(
-      `https://api.github.com/repos/${frm.doc.github_user}/${frm.doc.repository}/hooks`,
-      {
-        method: "POST",
-        headers: {
-          Authorization: `Bearer ${github_access_token}`,
+    if (frm.is_new()) {
+      const github_access_token = frappe.get_cookie("github_access_token");
+      const webhookSecret =
+        Math.random().toString(36).substring(2, 15) + +new Date();
+      const webhookData = {
+        name: "web",
+        active: true,
+        events: ["issues", "issue_comment", "label"],
+        config: {
+          url: `${window.location.origin}/api/method/github_sync.github_sync.api.webhook`,
+          content_type: "json",
+          insecure_ssl: "0",
+          secret: webhookSecret,
         },
-        body: JSON.stringify(webhookData),
-      }
-    );
-    const data = await response.json();
-    frm.set_value("github_webhook_id", data.id);
-    frm.set_value("github_webhook_secret", webhookSecret);
-    frm.set_value("github_access_token", github_access_token);
+      };
+
+      const response = await fetch(
+        `https://api.github.com/repos/${frm.doc.github_user}/${frm.doc.repository}/hooks`,
+        {
+          method: "POST",
+          headers: {
+            Authorization: `Bearer ${github_access_token}`,
+          },
+          body: JSON.stringify(webhookData),
+        }
+      );
+      const data = await response.json();
+      frm.set_value("github_webhook_id", data.id);
+      frm.set_value("github_webhook_secret", webhookSecret);
+      frm.set_value("github_access_token", github_access_token);
+    }
   },
 });
--- github_sync/github_sync/doctype/github_connection/github_connection.py
+++ github_sync/github_sync/doctype/github_connection/github_connection.py
@@ -7,5 +7,8 @@ from github_sync.utils.github import delete_github_webhook
 
 
 class GithubConnection(Document):
+	def validate(self):
+		if not self.is_new() and (self.has_value_changed('github_user') or self.has_value_changed('repository') or self.has_value_changed('project') or self.has_value_changed('github_access_token') or self.has_value_changed('github_webhook_id') or self.has_value_changed('github_webhook_secret')):
+			frappe.throw(_('You cannot change Github User, Repository, Project, Github Access Token, Github Webhook Secret or Github Webhook ID after creation'))
 	def on_trash(self):
 		delete_github_webhook(self.github_webhook_id, self.repository, self.github_user, self.github_access_token)
