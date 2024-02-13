56260a6c028bf9367401a19ad7e89621a3c1b582
Ashish Baravaliya <vier.institute35@gmail.com>
2024-02-12 23:42:29 -0500

    feat: file handler added

--- github_sync/utils/github.py
+++ github_sync/utils/github.py
@@ -1,3 +1,4 @@
+from urllib.parse import urlparse
 import frappe
 import requests
 import json
@@ -7,9 +8,47 @@ from bs4 import BeautifulSoup
 from markdown.extensions import Extension
 from markdown.preprocessors import Preprocessor
 import re
+import os
 
 github_user="githubuser@example.com"
 admin="Administrator"
+
+def fetch_image_and_save(image_url):
+    pattern = r'https?://github\.com/([^/]+)/([^/]+)'
+    match = re.search(pattern, image_url)
+    if not match:
+        return "Invalid Url"
+    
+    username, repo = match.groups()
+    connection = frappe.db.get_value("Github Connection", {"repository": repo, "github_user": username}, ["github_access_token"], as_dict=1)
+    if connection:
+        headers = {
+            'Authorization': f'token {connection.github_access_token}',
+            'Accept': 'application/vnd.github+json',
+            "X-GitHub-Api-Version": "2022-11-28"
+        }
+        parsed_url = urlparse(image_url)
+        filename = os.path.basename(parsed_url.path) + '.png'
+        
+        directory_path = frappe.get_site_path('public', 'files', 'github_sync', repo)
+        if not os.path.exists(directory_path):
+            os.makedirs(directory_path)  
+        
+        public_file_path = os.path.join(directory_path, filename)
+        if os.path.exists(public_file_path):
+            file_url = f"/files/github_sync/{repo}/{filename}"
+            return file_url
+        
+        response = requests.get(image_url, headers=headers)
+        if response.status_code == 200:
+            with open(public_file_path, 'wb') as file:
+                file.write(response.content)
+            file_url = f'/files/github_sync/{repo}/{filename}'
+            return file_url
+        else:
+            return "Failed to fetch image from GitHub"
+    
+    return "GitHub Access Token Not Found"
 
 class EnhancedMarkdownExtension(Extension):
     def extendMarkdown(self, md):
@@ -43,10 +82,20 @@ class EnhancedMarkdownPreprocessor(Preprocessor):
                 code_block_lines.append(f'<div class="ql-code-block">{line}</div>')
                 continue
 
+            elif re.match(r'!\[(.*?)\]\((.*?)\)', line):
+                image_match = re.match(r'!\[(.*?)\]\((.*?)\)', line)
+                alt_text = image_match.group(1)
+                image_url = image_match.group(2)
+                saved_image = fetch_image_and_save(image_url)
+                new_line = f'<img src="{saved_image}" alt="{alt_text}">'
+                new_lines.append(new_line + '  ')
+                continue
+            
             elif re.match(r'^#[^ ]', line):
                 content = line.lstrip('#').strip()
                 new_line = f'<p><a href="http://google.com" rel="noopener noreferrer">{content}</a></p>'
-                new_lines.append(new_line)
+                new_lines.append(new_line + '  ')
+                continue
 
             # Handle task lists
             elif re.match(r'- \[ \] .+', line):
@@ -110,7 +159,7 @@ def delete_github_webhook(webhook_id, repo, user, access_token):
         'Accept': 'application/vnd.github+json',
         "X-GitHub-Api-Version": "2022-11-28"
     }
-    response = requests.delete(url, headers=headers)
+    requests.delete(url, headers=headers)
 
 def handle_issue_comment_webhook(data, connection):
     comment = data.get("comment").get("body")
@@ -168,6 +217,7 @@ def handle_issue_webhook(data, connection):
                 "github_sync_github_user": connection.github_user,
             })
             task.save(ignore_permissions=True)
+        frappe.db.commit()
     except Exception as e:
         return "Error handling issue webhook: " + str(e)
     finally:
